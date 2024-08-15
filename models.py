1501eefc9593db4967dd022ec56dfd056ff30260
devanshrj <d7@notdiamond.ai>
2024-08-15 17:23:57 +0000

    updated setup and reqs

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import google.generativeai as genai
 import litellm
 
 import dotenv
@@ -16,36 +15,6 @@ class BaseLLM(ABC):
         Generate num_samples number of completions for a given input prompt.
         """
         pass
-
-
-class GeminiLLM(BaseLLM):
-    """
-    Wrapper for Gemini language models.
-    """
-
-    def __init__(self, model_name: str, **kwargs) -> None:
-        """
-        Initialize a Gemini model with the given API key.
-        """
-        api_key = kwargs['api_key']
-        genai.configure(api_key=api_key)
-        self.model_path = model_name
-        self.model = genai.GenerativeModel(model_name)
-
-    def generate_completions(self, input_prompt: str, **kwargs) -> list[str]:
-        sample_completions = []
-        n_sample = kwargs.get("n_sample", 1)
-        for _ in range(n_sample):
-            response = self.model.generate_content(input_prompt)
-            # response.text sometimes throws a ValueError because model output is stored in response.parts instead
-            try:
-                completion = response.text
-            except ValueError:
-                completion = response.parts
-            # 'completion': [] is returned when the model fails to generate a completion -> breaks JSON formatting
-            completion = str(completion)
-            sample_completions.append(completion)
-        return sample_completions
 
 
 ND2LITELLM = {
