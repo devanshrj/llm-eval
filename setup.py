9f25b9deff9b0d879371450d80b52965cb449e47
devanshrj <d7@notdiamond.ai>
2024-08-15 21:25:46 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -11,10 +11,15 @@ setup(
     description="",
     author="OpenAI",
     packages=find_packages(),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
+    # install_requires=[
+    #     str(r)
+    #     for r in pkg_resources.parse_requirements(
+    #         open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+    #     )
+    # ],
+    install_requires=['litellm', 'human-eval', 'humaneval-x'],
+    dependency_links=[
+        ''.join(['file:\\', os.path.join(os.getcwd(), 'human-eval#egg=human-eval-1.0')]),
+        ''.join(['file:\\', os.path.join(os.getcwd(), 'humaneval-x#egg=humaneval-x-1.0')])
+    ]
 )
