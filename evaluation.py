61ca5ecfd41d81bf581a2eec4ab4132ec55c99a1
devanshrj <d7@notdiamond.ai>
2024-08-16 00:30:04 +0000

    forgot generation code lol

--- evaluation.py
+++ evaluation.py
@@ -158,29 +158,29 @@ class HumanEvalX(Evaluator):
         samples = []
 
         log.info("Generating samples...")
-        # progress_bar = tqdm(total=len(dataset) * n_sample,
-        #                     desc="Generating samples")
+        progress_bar = tqdm(total=len(dataset) * n_sample,
+                            desc="Generating samples")
 
-        # for task_id in dataset:
-        #     language = task_id.split("/")[0].lower()
-        #     og_prompt = dataset[task_id]["prompt"]
-        #     if use_template:
-        #         prompt = self.create_prompt(og_prompt, language)
-        #     completions = model.generate_completions(prompt, n_sample=n_sample)
-        #     for completion in completions:
-        #         if post_process:
-        #             completion = self.process_code(completion, language)
-        #         sample = dict(task_id=task_id,
-        #                       generation=completion,
-        #                       prompt=og_prompt)
-        #         samples.append(sample)
-        #     progress_bar.update(n_sample)
-        # progress_bar.close()
+        for task_id in dataset:
+            language = task_id.split("/")[0].lower()
+            og_prompt = dataset[task_id]["prompt"]
+            if use_template:
+                prompt = self.create_prompt(og_prompt, language)
+            completions = model.generate_completions(prompt, n_sample=n_sample)
+            for completion in completions:
+                if post_process:
+                    completion = self.process_code(completion, language)
+                sample = dict(task_id=task_id,
+                              generation=completion,
+                              prompt=og_prompt)
+                samples.append(sample)
+            progress_bar.update(n_sample)
+        progress_bar.close()
 
         log.info("Storing samples...")
         model_name = model.model_name.replace("/", "_")
         pred_filename = f"{out_path}/{model_name}_predictions.jsonl"
-        # write_jsonl(pred_filename, samples)
+        write_jsonl(pred_filename, samples)
 
         log.info("Evaluating samples...")
         tmp_path = f"{os.getcwd()}/executions/{model_name}"
